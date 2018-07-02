72503d5cca58b4ba4754e6cb5c9e28fd51895aa9
tbfikrif <tbfikrif@gmail.com>
2018-07-02 11:17:18 +0700

    Implementasi QR Code Scanner

--- app/src/main/java/com/kpunikom/absenhadirkinest/MainActivity.java
+++ app/src/main/java/com/kpunikom/absenhadirkinest/MainActivity.java
@@ -1,13 +1,82 @@
 package com.kpunikom.absenhadirkinest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String EXTRA_MESSAGE = "com.kpunikom.absenhadirkinest.MESSAGE";
+
+    // View Object
+    private Button buttonScan;
+    private TextView textViewNama, textViewEmail;
+
+    //qr code scanner object
+    private IntentIntegrator intentIntegrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize object
+        buttonScan = findViewById(R.id.buttonScan);
+        textViewNama = findViewById(R.id.textViewNama);
+        textViewEmail = findViewById(R.id.textViewEmail);
+
+        // attaching onclickListener
+        buttonScan.setOnClickListener(this);
+    }
+
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(this, "Hasil tidak ditemukan", Toast.LENGTH_SHORT).show();
+            }else{
+                // jika qrcode berisi data
+                try{
+                    // converting the data json
+                    JSONObject object = new JSONObject(result.getContents());
+                    // atur nilai ke textviews
+                    textViewNama.setText(object.getString("nama"));
+                    textViewEmail.setText(object.getString("email"));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    // jika format encoded tidak sesuai maka hasil
+                    // ditampilkan ke toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // inisialisasi IntentIntegrator(scanQR)
+        intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.initiateScan();
     }
 }
